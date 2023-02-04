6ebae190c79e8347ef3d3653cadc9b6540f1a585
abuzova <alla.abuzova@gmail.com>
2023-02-04 01:29:51 +0100

    version 1.0 main menu

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    //viewportWidth: 1600,
+    viewportWidth: 2200,
+    viewportHeight: 1200,
   },
 });
